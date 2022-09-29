422d0b35018622479caffb9bf54ecafee04eeb11
chanyDev <chanyd.dev@gmail.com>
2022-09-29 19:47:35 +0900

    FEAT : 애니메이션 추가 정보 레이아웃 구현

--- components/animation/organisms/animation-content-layout.tsx
+++ components/animation/organisms/animation-content-layout.tsx
@@ -1,17 +1,13 @@
 import styled from '@emotion/styled';
 import AnimationCover from '../atoms/animation-cover';
 import AnimationContent from '../molecules/animation-content';
-import AnimationStatusLayout from './animation-status-layout';
 
 const AnimationContentLayout = () => {
   return (
-    <div style={{ width: '80%', margin: '0 auto' }}>
-      <StyledContentLayout>
-        <AnimationCover cover="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-C6FPmWm59CyP.jpg" />
-        <AnimationContent />
-      </StyledContentLayout>
-      <AnimationStatusLayout />
-    </div>
+    <StyledContentLayout>
+      <AnimationCover cover="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-C6FPmWm59CyP.jpg" />
+      <AnimationContent />
+    </StyledContentLayout>
   );
 };
 
