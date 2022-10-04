f12ef1645808266dd408326e75a24746ae2b1908
LEE JINWOO <70833362+wlsdn1101@users.noreply.github.com>
2022-10-04 11:40:39 +0900

    Update skeleton-ani-list-item.tsx

--- components/mainpage/molecules/skeleton-ani-list-item.tsx
+++ components/mainpage/molecules/skeleton-ani-list-item.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
 import styled from '@emotion/styled';
 
 import SkeletonImg from '../atoms/skeleton-image';
 import SkeletonName from '../atoms/skeleton-name';
 
-const SkeletonAniBox = styled.div`
+const SkeletonAniContainer = styled.div`
   cursor: pointer;
   width: 185px;
 `;
 
 const SkeletonAniListItem = () => {
   return (
-    <SkeletonAniBox>
+    <SkeletonAniContainer>
       <SkeletonImg />
       <SkeletonName />
-    </SkeletonAniBox>
+    </SkeletonAniContainer>
   );
 };
 
