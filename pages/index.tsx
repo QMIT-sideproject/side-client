0a8302126f1dba199864bb26df896d0f2c936035
LEE JINWOO <70833362+wlsdn1101@users.noreply.github.com>
2022-10-04 11:36:21 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import type { NextPage } from 'next';
 import styled from '@emotion/styled';
 import MainLayout from '../components/mainpage/templates/layout-main';
 
-const MainpageContainer = styled.div`
+const MainpageWrapper = styled.div`
   width: 100%;
   height: 100vh;
 `;
 
 const MainPage: NextPage = () => {
   return (
-    <MainpageContainer>
+    <MainpageWrapper>
       <MainLayout />
-    </MainpageContainer>
+    </MainpageWrapper>
   );
 };
 
