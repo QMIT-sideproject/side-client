f3cdb152b26e9b083769c432f949a78aa1eb3fb9
chanyDev <chanyd.dev@gmail.com>
2022-10-06 11:41:54 +0900

    REFACTOR : 날짜 변환 함수 리팩토링

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const stringToDateFormat = (date: string) => {
-  return moment(date).format('YYYY-MM-DD');
+export const stringToDateFormat = (year: number, month: number, day: number) => {
+  return moment(`${year}-${month}-${day}`).format('YYYY-MM-DD');
 };
