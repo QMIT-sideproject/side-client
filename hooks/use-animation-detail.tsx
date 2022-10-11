07881ab4d090dac4c493ffcda8548c7166726ce1
chanyDev <chanyd.dev@gmail.com>
2022-10-11 16:54:28 +0900

    FEAT : 페이지네이션을 위한 파라미터 추가

--- hooks/use-animation-detail.tsx
+++ hooks/use-animation-detail.tsx
@@ -1,11 +1,13 @@
 import { useQuery } from '@apollo/client';
 import { AnimationDetailData, AnimationDetailVars, GET_ANIMATION_DETAIL } from '@components/animation/templates/detail-query';
 
-const useAnimationDetail = (id: string | string[] | undefined, version: number) => {
+const useAnimationDetail = (id: string | string[] | undefined, version: number, page: number = 1, perPage: number = 6) => {
   const { data, loading } = useQuery<AnimationDetailData, AnimationDetailVars>(GET_ANIMATION_DETAIL, {
     variables: {
       mediaId: Number(id),
       version: version,
+      page,
+      perPage,
     },
   });
 
