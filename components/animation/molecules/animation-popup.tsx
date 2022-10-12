5fb18388bae485e8d06670583ad3b96a45370e28
chanyDev <chanyd.dev@gmail.com>
2022-10-12 16:52:41 +0900

    STYLE : 고정 너비 추가

--- components/animation/molecules/animation-popup.tsx
+++ components/animation/molecules/animation-popup.tsx
@@ -36,6 +36,7 @@ const PopupContainer = styled.div<Pick<Props, 'position'>>`
   background-color: #fff;
   pointer-events: none;
   transition: 0.15s ease-in-out;
+  width: 220px;
 `;
 
 export default AnimationPopup;
