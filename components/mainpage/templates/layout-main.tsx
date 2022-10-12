42ec5264c3029a6ba980cd55cc66555cc0537cc3
이진우 <ljw041101@gmail.com>
2022-10-12 12:24:10 +0900

    FEAT : animation 무한 스크롤 구현

--- components/mainpage/templates/layout-main.tsx
+++ components/mainpage/templates/layout-main.tsx
@@ -2,19 +2,19 @@ import { ApolloError } from '@apollo/client';
 import AniList from '../organisms/animation-list';
 import SearchNavBar from '../organisms/search-bar';
 import SkeletonAniList from '../organisms/skeleton-list';
-import { DataType } from './main-query';
+import { AniType } from './main-query';
 
 interface Props {
-  data?: DataType | undefined;
+  data?: AniType[];
   loading?: boolean;
   error: ApolloError | undefined;
 }
 
-const MainLayout = ({data, loading, error} : Props) => {
+const MainLayout = ({ data, loading, error }: Props) => {
   return (
     <>
       <SearchNavBar />
-      {loading ? <SkeletonAniList /> : (error || !data) ? <div>error</div> : <AniList animations={data?.Page?.media} />}
+      {loading ? <SkeletonAniList /> : error || !data ? <div>error</div> : <AniList animations={data} />}
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 import MainLayout from '../components/mainpage/templates/layout-main';
 import { useGetAnimations } from 'hooks/get-animations';
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
-import { DataType } from 'components/mainpage/templates/main-query';
+import { useEffect, useState, useRef } from 'react';
+import { AniType } from 'components/mainpage/templates/main-query';
 
 const MainpageWrapper = styled.div`
   width: 100%;
@@ -11,11 +11,36 @@ const MainpageWrapper = styled.div`
 `;
 
 const MainPage: NextPage = () => {
-  const { data, loading, error } = useGetAnimations(1);
+  const [page, setPage] = useState(1);
+  const { data, loading, error } = useGetAnimations(page);
+  const lastRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const [animation, setAnimation] = useState<AniType[]>([]);
+
+  useEffect(() => {
+    let io: IntersectionObserver | undefined = undefined;
+
+    if (data?.Page.media?.length) {
+      io = new IntersectionObserver(
+        (entries, observer) => {
+          const target = entries[0];
+
+          if (target.isIntersecting) {
+            setPage((page) => page + 1);
+            setAnimation((ani) => ani?.concat(data.Page.media));
+
+            observer.disconnect();
+          }
+        },
+        { threshold: 1 },
+      );
+      io.observe(lastRef.current);
+    }
+  }, [animation, data]);
 
   return (
     <MainpageWrapper>
-      <MainLayout data={data} loading={loading} error={error} />
+      <MainLayout data={animation} loading={loading} error={error} />
+      <div ref={lastRef}></div>
     </MainpageWrapper>
   );
 };
