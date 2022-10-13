1534be2485a0fc98c8795e1bd890f120297145c0
이진우 <ljw041101@gmail.com>
2022-10-13 15:38:02 +0900

    FEAT : 메인페이지 애니메이션 팝업 추가

--- components/mainpage/molecules/styles.ts
+++ components/mainpage/molecules/styles.ts
@@ -1,6 +1,17 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
 
+export const AnimationListItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+
+  position: relative;
+  &:hover .popup {
+    opacity: 1;
+  }
+`;
+
 export const AnimationDetailLink = styled(Link)`
   cursor: pointer;
   width: 185px;
